a3630523b3753de86c41d9e160d1710be991701a
PoneyRose <mathis.laurent.3m@gmail.com>
2019-02-10 17:50:24 +0100

    Initialisation

---
+++ index.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <title>tpe</title>
+    <link rel="stylesheet" type="text/css" href="site_.css">
+    <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+
+</head>
+
+<body id="body">
+    <header>
+        <ul id="menu">
+            <li >
+                <a class="choix" href="C:\Users\thomas\Desktop\site tpe\site_credit">
+                    Accueil
+                </a>
+            </li>
+            <li >
+                <a class="choix" href="C:\Users\thomas\Desktop\site tpe">
+                    Experience
+                </a>
+            </li>
+            <li >
+                <a class="choix" href="C:\Users\thomas\Desktop\site tpe">
+                    Crédit
+                </a>
+            </li>
+        </ul>
+    </header>
+    <div>
+    <div id="pgrh1">
+        <marquee behavior="slide" direction="right" scrollamount="10" style="font-size:25px ;" loop=999999 >
+        / Texte à entrer /
+        </marquee>
+
+            <div>
+                <ul id="sommaire">
+                    <li class="titsommaireans">
+            I.système de stockage
+                    </li>
+            carte perforée
+                    <li class="sommaireans">
+            bande magnétique
+                    </li>
+                    <li class="sommaireans">
+            cassette audio
+                    </li>
+                    <li class="sommaireans">
+            vinyl
+                    </li>
+                    <li class="sommaireans">
+            disquette
+                    </li>
+                    <li class="sommaireans">
+            DVD
+                    </li>
+                    <li class="sommaireans">
+            mémoire flash
+                    </li>
+                    <li class="titsommaireans">
+            II.trasnfert d'informations
+                    </li>
+                    <li class="sommaireans">
+            cablé
+                    </li>
+                    <li class="sommaireans">
+            fibré
+                    </li>
+                    <li class="sommaireans">
+            fréquence radio
+                    </li>
+                    <li class="sommaireans">
+            cellulaire
+                    </li>
+                    <li class="sommaireans">
+            wifi
+                    </li>
+                    <li class="sommaireans">
+            serveur
+                    </li>
+                    <li class="sommaireans">
+            internet
+                    </li>
+                    <li class="sommaireans">
+            block chain
+                    </li>
+                </ul>
+            </div> 
+
+    </div>   
+    </div>
+    <div id="next">
+        <a id="next_button">Suivant</a>
+    </div>
+
+    <div>
+<?php
+    $dir = opendir('.');
+    while (($file = readdir($dir)) !== false) {
+        echo "File: {$file}</br>\n";
+    }
+?>            
+    </div>
+</body>
+</html>
+
+
---
+++ site_credit.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<head>
+    <link type="text/css" src="site_.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+
+</head>
+
+<body>
+    <header>
+        <div class="menu">
+            <a href="C:\Users\thomas\Desktop\site tpe\site_credit">credit</a>
+        </div>
+        <div class="menu">
+            <a href="C:\Users\thomas\Desktop\site tpe">experience</a>
+        </div>
+        <div class="menu">
+            <a href="C:\Users\thomas\Desktop\site tpe">accueil</a>
+        </div>
+    </header>
+    <div id="clement">
+        clement
+    </div>
+    <div id="mathis">
+        mathis
+    </div>
+    <div id="thomas">
+        thomas
+    </div>
+    <div id="baptiste">
+        baptiste
+    </div>
+    <div id="sources">
+        sources
+    </div>    
+</body>
